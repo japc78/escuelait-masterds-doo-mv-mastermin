4faee906adfb79da386c45d1da3f6b53bf43b638
japc <japc.grafico@gmail.com>
2020-12-05 19:46:17 +0100

    Implement Mastermind class.

---
+++ src/src/instanceofteam/mastermind/Mastermind.java
@@ -0,0 +1,22 @@
+package instanceofteam.mastermind;
+
+import instanceofteam.mastermind.models.Game;
+import instanceofteam.mastermind.views.View;
+
+public class Mastermind {
+    private Game game;
+    private View view;
+
+    Mastermind() {
+        this.game = new Game();
+        this.view = new View(this.game);
+    }
+
+    private void play() {
+        this.view.interact();
+    }
+
+    public static void main(String[] args) {
+        new Mastermind().play();
+    }
+}
