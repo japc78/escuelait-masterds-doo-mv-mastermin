e056aa2b881055ed2e20b6b3a3d509568916bd1b
japc <japc.grafico@gmail.com>
2020-12-05 19:47:56 +0100

    Implement View class

---
+++ src/src/instanceofteam/mastermind/views/View.java
@@ -0,0 +1,24 @@
+package instanceofteam.mastermind.views;
+
+import instanceofteam.mastermind.models.Game;
+
+public class View {
+	private Game game;
+	private PlayView playView;
+	private StartView startView;
+	private ResumeView resumeView;
+
+	public View (Game game) {
+		this.game = game;
+		this.playView = new PlayView(this.game);
+		this.startView = new StartView(this.game);
+		this.resumeView = new ResumeView(this.game);
+	}
+
+	public void interact() {
+		do {
+			this.startView.interact();
+			this.playView.interact();
+		} while (this.resumeView.interact());
+	}
+}
