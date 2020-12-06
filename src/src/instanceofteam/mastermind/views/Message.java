893bb06eabf10df1108822b159f7e4e203a06884
japc <japc.grafico@gmail.com>
2020-12-06 11:35:04 +0100

    Implements Message Enum

---
+++ src/src/instanceofteam/mastermind/views/Message.java
@@ -0,0 +1,31 @@
+package instanceofteam.mastermind.views;
+
+enum Message {
+    START_TITLE("----- Mastermind --------"),
+    ATTEMP_CAPTION("attemp(s):"),
+    COMBINATION_ENTER("Propose a combination: "),
+    COMBINATION_WRONG_COLOR("Wrong colors they must be: "),
+    COMBINATION_WRONG_LENGTH("Wrong proposed combination length"),
+    COMBINATION_WRONG_DUPLICATE_COLOR("Wrong proposed, can not repeat color."),
+    RESULT_BLACKS("blacks and"),
+    RESULT_WHITES("whites"),
+    WINNER("You've won!!!"),
+    LOSER("You've lost"),
+    RESUMEN("Resume? (y/n)");
+
+    private String message;
+
+    private Message(String message) {
+        this.message=message;
+    }
+
+    @Override
+	public String toString() {
+		return this.message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Message.START_TITLE.toString());
+    }
+
+}
