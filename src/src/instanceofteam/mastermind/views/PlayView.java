cefa0473d59a4b81bd271d34238b9dea3781807f
japc <japc.grafico@gmail.com>
2020-12-07 14:59:26 +0100

    Implement features on PlayView

--- src/src/instanceofteam/mastermind/views/PlayView.java
+++ src/src/instanceofteam/mastermind/views/PlayView.java
@@ -12,7 +12,19 @@ public class PlayView extends SubView{
         Console console = new Console();
         String combination;
         do {
-            console.write(Message.ATTEMP_CAPTION.toString());
+            console.write("Secret: " + this.game.getSecretCombination());
+            console.write(this.game.getCurrentNumAttempts() + " "
+                + Message.ATTEMP_CAPTION.toString());
+
+            for (String result : this.game.getAttemptsResult()) {
+                String combinationString = result.split(",")[0];
+                String blacks = result.split(",")[1];
+                String whites = result.split(",")[2];
+
+                console.write(combinationString + Message.RESULT_SEPARATOR +
+                    blacks + Message.RESULT_BLACKS +
+                    whites + Message.RESULT_WHITES);
+            }
 
             do {
                 combination = console.read(Message.COMBINATION_ENTER.toString());
@@ -20,14 +32,18 @@ public class PlayView extends SubView{
 
             this.game.addCombination(combination);
 
-        } while (!this.game.isMastermind());
+            if (this.game.isWinner()) {
+                console.write(Message.WINNER.toString());
+            }
+
+        } while (this.game.isMastermind());
     }
 
     private boolean isValidCombination(String combination) {
         Console console = new Console();
         int length = combination.length();
 
-        if (length != this.game.getNumAttempts()) {
+        if (length != this.game.getLengthCombination()) {
             combination = console.read(Message.COMBINATION_WRONG_LENGTH.toString());
             return false;
         }
@@ -37,10 +53,21 @@ public class PlayView extends SubView{
             return false;
         }
 
-        if (this.game.containRepeats(combination)) {
+        if (containRepeats(combination)) {
             combination = console.read(Message.COMBINATION_WRONG_DUPLICATE_COLOR.toString());
             return false;
         }
         return true;
+    }
+
+    private boolean containRepeats(String combination) {
+        String tmp = "";
+        for (char letterColor : combination.toCharArray()) {
+            if (tmp.contains(String.valueOf(letterColor))) {
+                return true;
+            }
+            tmp = tmp.concat(String.valueOf(letterColor));
+        }
+        return false;
     }
 }
